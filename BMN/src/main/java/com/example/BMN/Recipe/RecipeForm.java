35e526fbbfb19987c8ad1261041973ca36ea6c56
UngsikJo <ungsikjo0601@gmail.com>
2025-09-20 21:32:14 +0900

    test

--- BMN/src/main/java/com/example/BMN/Recipe/RecipeForm.java
+++ BMN/src/main/java/com/example/BMN/Recipe/RecipeForm.java
@@ -11,7 +11,7 @@ import java.util.List;
 
 @Getter
 @Setter
-public class RecipeForm {
+public class RecipeForm { //
     @NotEmpty(message="제목은 필수항목입니다.")
     @Size(max=200)
     private String subject;
